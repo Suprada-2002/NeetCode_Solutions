4b6cd4e6d41df9c705b2aee333f6291f66259592
Suprada Tripathy <87217681+Suprada-2002@users.noreply.github.com>
2025-11-08 22:12:45 +0530

    solution

--- Arrays_&_Hashing/Contains_Duplicate.java
+++ Arrays_&_Hashing/Contains_Duplicate.java
@@ -1,1 +1,14 @@
-re
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        if(nums.length == 1) return false;
+
+        HashSet<Integer> map = new HashSet<>();
+
+        for(int i: nums){
+            if(map.contains(i)) return true;
+            map.add(i);
+        }
+        return false;
+        
+    }
+}
